795effe2e63fbf3645844d8adfdb4b4441352910
yagodaoud <yagodaoud@hotmail.com>
2024-09-14 16:19:02 -0300

    Fix: add cache

--- app/src/main/java/com/yagodaoud/cryptotrack/data/viewmodel/CryptoViewModel.kt
+++ app/src/main/java/com/yagodaoud/cryptotrack/data/viewmodel/CryptoViewModel.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yagodaoud.cryptotrack.data.model.CryptoCurrency
 import com.yagodaoud.cryptotrack.data.repository.CryptoRepository
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import java.time.Instant
 
 class CryptoViewModel(private val repository: CryptoRepository) : ViewModel() {
     private val _cryptoList = MutableStateFlow<List<CryptoCurrency>?>(null)
@@ -15,15 +17,35 @@ class CryptoViewModel(private val repository: CryptoRepository) : ViewModel() {
     private val _isLoading = MutableStateFlow(false)
     val isLoading: StateFlow<Boolean> = _isLoading
 
+    private var lastFetchTime: Instant? = null
+    private val cacheDuration = 2 * 60
+
     fun fetchCryptoPrices(apiKey: String, currency: String) {
+        _isLoading.value = true
+
+        if (isCacheValid()) {
+            viewModelScope.launch {
+                delay(200)
+                _isLoading.value = false
+            }
+            return
+        }
+
         viewModelScope.launch {
-            _isLoading.value = true
             try {
                 val data = repository.getCryptoPrices(apiKey, currency)
                 _cryptoList.value = data
+                lastFetchTime = Instant.now()
             } finally {
                 _isLoading.value = false
             }
         }
+    }
+
+    private fun isCacheValid(): Boolean {
+        val now = Instant.now()
+        return lastFetchTime?.let {
+            now.epochSecond - it.epochSecond < cacheDuration
+        } ?: false
     }
 }
